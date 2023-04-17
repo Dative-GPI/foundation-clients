2ec41cb4fa1aab7252024de9d34944ea40844ac3
Quentin SCHROTER <qschroter@dative-gpi.com>
2023-04-17 22:05:06 +0200

    add missing declaration in interfaces

--- Foundation.Clients/Abstractions/IAdminFoundationClient.cs
+++ Foundation.Clients/Abstractions/IAdminFoundationClient.cs
@@ -6,8 +6,10 @@ namespace Foundation.Clients.Abstractions
     {
         void Init(IFoundationClient root);
 
+        IAdminDeviceFoundationClient Devices { get; }
         IAdminDeviceTwinFoundationClient DeviceTwin { get; }
         IAdminDeviceOrganisationFoundationClient DeviceOrganisations { get; }
+        IAdminDeviceSourceFoundationClient DeviceSources { get; }
         IAdminUserApplicationFoundationClient UserApplications { get; }
         IAdminPermissionFoundationClient Permissions { get; }
         IAdminSourceFoundationClient Sources { get; }
